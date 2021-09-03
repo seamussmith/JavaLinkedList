bf8709352f42a2a0369efefcc87b43771d868ded
seamussmith <student.141504@worcesterschools.net>
2021-09-03 19:02:41 -0400

    New Testing code

--- src/main/App.java
+++ src/main/App.java
@@ -4,12 +4,28 @@ public class App
 {
     public static void main(String[] args) 
     {
-        var list = new LinkedList<>(1);
+        System.out.println("Normal list: ");
+        var normalList = new LinkedList<>(1);
+
         for (int i = 2; i != 10; ++i)
-            list.add(i);
+            normalList.add(i);
+
+        System.out.println(normalList);
+        var inversedNormalList = normalList.inverse();
+        System.out.println(inversedNormalList);
         
-        System.out.println(list);
-        var inversed = list.inverse();
-        System.out.println(inversed);        
+        System.out.println("-----------------------------");
+
+        System.out.println("Self-Linking list:");
+        var selfLinkingList = new LinkedList<>(1);
+        var tail = selfLinkingList;
+
+        for (int i = 2; i != 10; ++i)
+            tail = selfLinkingList.add(i);
+        tail.next = selfLinkingList;
+
+        System.out.println(selfLinkingList);
+        var inversedSelfLinkingList = selfLinkingList.inverse();
+        System.out.println(inversedSelfLinkingList);
     }
 }
