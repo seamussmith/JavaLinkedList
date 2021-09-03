b1144e2feda7fa6ab7b9532a72c4960f11bc3d52
seamussmith <student.141504@worcesterschools.net>
2021-09-02 21:25:30 -0400

    newline to seperate logic

--- src/com/App.java
+++ src/com/App.java
@@ -7,6 +7,7 @@ public class App
         var list = new LinkedList<>(1);
         for (int i = 2; i != 11; ++i)
             list.append(i);
+        
         System.out.println(list);
         var newhead = list.inverse();
         System.out.println(newhead);
