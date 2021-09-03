037599ecbb8223bd0a4d95978c38d22eb962c34d
seamussmith <student.141504@worcesterschools.net>
2021-09-02 21:09:16 -0400

    Comment up the LinkedList class

--- src/com/LinkedList.java
+++ src/com/LinkedList.java
@@ -11,21 +11,29 @@ public class LinkedList<T>
         this.data = data;
     }
 
+    // Get the n-th node of the LinkedList
     public LinkedList<T> index(int idx)
     {
+        // Loop until i is the index.
+        // If i is zero, the for loop just doesn't run and it just returns the linked list itself
         var curr = this;
         for (int i = 0; i != idx; ++i)
         {
+            // If the next node is null, then the index is out of bounds. If we kept going, we would dereference a nullptr on the next itteration.
             if (curr.next == null)
                 throw new IndexOutOfBoundsException();
+            // Get the next node
             curr = curr.next;
         }
+        // Once we exit the loop, curr will be the n-th nod.
         return curr;
     }
     
+    // Sets the next property of the last node to a new node with the given data
     public void append(T data)
     {
         var newLink = new LinkedList<>(data);
+        // Find the last node of the linked list
         var curr = this;
         while (curr.next != null)
             curr = curr.next;
@@ -53,6 +61,7 @@ public class LinkedList<T>
         LinkedList<T> prev = null;
         while (curr != null)
         {
+            // Preserve the original order of the two nodes
             var first = curr;
             var second = curr.next;
 
